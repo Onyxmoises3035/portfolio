ffee89557bc854660e61528430c20b0ee4ff8b55
Onyxmoises3035 <maumoy1229@gmail.com>
2024-11-04 15:02:49 -0600

    load images

--- src/app/page.js
+++ src/app/page.js
@@ -20,6 +20,14 @@ const Home = () => {
     <div className="flex content-center justify-center size-full containerColor">
       <div className="opacity-0 timeNight"></div>
       <div className="opacity-0 timeDay"></div>
+      <div className="opacity-0 bg-[url('/images/day.png')]"></div>
+      <div className="opacity-0 bg-[url('/images/n01.png')]"></div>
+      <div className="opacity-0 bg-[url('/images/n02.png')]"></div>
+      <div className="opacity-0 bg-[url('/images/n03.png')]"></div>
+      <div className="opacity-0 bg-[url('/images/night.png')]"></div>
+      <div className="opacity-0 bg-[url('/images/d01.png')]"></div>
+      <div className="opacity-0 bg-[url('/images/d02.png')]"></div>
+      <div className="opacity-0 bg-[url('/images/d03.png')]"></div>
       <Bar lan={lan} change={changeLan}/>
       <div>
         <About_me lan={lan} />
